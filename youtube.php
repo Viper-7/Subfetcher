8e744349cd37fb559f80d56e48cb2cfc5ede3edb
viper-7 <viper7@viper-7.com>
2015-06-08 19:32:37 +1000

    ADDED: More thorough "Channel Scan" method ADDED: Running download monitoring (linux only) ADDED: Automatic download resuming ADDED: Video type column to listing ADDED: Filter videos by type ADDED: Alert sound when attention is required ADDED: Download videos manually by ID FIXED: Page refreshes while selecting items FIXED: OAuth tokens only last an hour

--- youtube.php
+++ youtube.php
@@ -23,8 +23,14 @@ $youtubedl_path = 'youtube-dl';
 // Google API path. If not found the script will attempt to install it
 $googleapi_path = 'google-api-php-client';
 
-// Show Recommendations from the home page? or only subscriptions if false.
-$show_recommendations = false;
+// Check the user's home page activity feed for new items, tends to show a random list of "some" new content
+$use_activity_scan = true;
+
+// Types of actiivty post to display, valid types are: bulletin, channelItem, comment, favourite, like, playlistItem, recommendation, social, subscription, upload
+$types = array('upload','recommendation','bulletin');
+
+// Check each of the user's subscribed channels for new uploads (slow)
+$use_channel_scan = false;
 
 
 /*
@@ -49,6 +55,9 @@ $OAUTH2_CLIENT_SECRET = 'changeme';
  */
 session_start();
 $message = '';
+$downloading = '';
+$formats = array(18 => '360p', 22 => '720p', 5 => '240p');
+$videos = array();
 
 if(!file_exists($googleapi_path . '/src/Google/autoload.php')) {
 	@exec('git clone --depth 1 https://github.com/google/google-api-php-client.git ' . $googleapi_path);
@@ -78,22 +87,37 @@ if(!file_exists($googleapi_path . '/src/Google/autoload.php')) {
 		if(!is_executable($youtubedl_path)) {
 			$message .= '<p>youtube-dl could not be found or installed. Videos cannot be downloaded</p>';
 		} else {
-			if(isset($_POST['download'])) {
-				$i=0;
+			if(!file_exists($video_path)) mkdir($video_path);
+			chdir($video_path);
+			
+			$running = shell_exec('ps aux | grep youtube-dl');
+			if($running) {
+				$count = substr_count($running, "\n") - 2;
+				if($count > 0)
+					$download_threads -= $count;
 				
-				if(!file_exists($video_path)) mkdir($video_path);
-				chdir($video_path);
+				if($download_threads < 1)
+					$download_threads = 1;
+				
+				$downloading = "<p>{$count} video(s) currently downloading</p>";
+			}
+
+			$files = glob($video_path . '/*.part');
+			$files = array_combine($files, array_map('filesize', $files));
+			
+			$oldfiles = @file_get_contents(__DIR__ . '/.youtube.parts');
+			$oldfiles = json_decode($oldfiles, true);
+			if(!$oldfiles) $oldfiles = array();
+			
+			if($oldfiles) {
 				$cmd = array();
-				foreach($_POST['download'] as $id => $download) {
-					if($download) {
-						$url = "http://www.youtube.com/watch?v={$id}";
-						$queued = file_get_contents(__DIR__ . '/.youtube.downloaded');
-						$queued = json_decode($queued, true);
-						if(!$queued) $queued = array();
-						$queued[$id] = true;
-						file_put_contents(__DIR__ . '/.youtube.downloaded', json_encode($queued));
-						$cmd[] = "{$youtubedl_path} {$url} -f 18 2>/dev/null >/dev/null";
-						$i++;
+				foreach($files as $file => $size) {
+					if(isset($oldfiles[$file]) && $oldfiles[$file] == $size && time() - filemtime($file) > 30) {
+						// Download has failed, resume it
+						if(preg_match('/-(\w+)\.\w+\.part$/', $file, $match)) {
+							$url = "http://www.youtube.com/watch?v=" . $match[1];
+							$cmd[] = "{$youtubedl_path} {$url} -f 18 3>/dev/null 2>/dev/null >/dev/null";
+						}
 					}
 				}
 				
@@ -106,122 +130,192 @@ if(!file_exists($googleapi_path . '/src/Google/autoload.php')) {
 					foreach($batch as $cmd) {
 						exec(implode(' && ', $cmd) . ' & disown');
 					}
-					$message .= "<p>Queued {$i} videos in " . count($batch) . " threads</p>";
+					$message .= "<p>Resumed {$i} stalled video(s) in " . count($batch) . " threads</p>";
 				}
+				
 			}
 			
-			if(isset($_POST['crap'])) {
-				$i=0;
-				foreach($_POST['crap'] as $id => $download) {
-					if($download) {
-						$queued = file_get_contents(__DIR__ . '/.youtube.hidden');
+			file_put_contents(__DIR__ . '/.youtube.parts', json_encode($files));
+			
+			if(!empty($_POST['videoid'])) {
+				$cmd = array();
+				foreach(explode(',', $_POST['videoid']) as $id) {
+					if(ctype_alnum($id)) {
+						$url = "http://www.youtube.com/watch?v={$id}";
+						$queued = @file_get_contents(__DIR__ . '/.youtube.downloaded');
 						$queued = json_decode($queued, true);
 						if(!$queued) $queued = array();
 						$queued[$id] = true;
-						file_put_contents(__DIR__ . '/.youtube.hidden', json_encode($queued));
-						$i++;
+						file_put_contents(__DIR__ . '/.youtube.downloaded', json_encode($queued));
+						$cmd[] = "{$youtubedl_path} {$url} -f 18 3>/dev/null 2>/dev/null >/dev/null";
 					}
 				}
 				
-				if($i)
-					$message .= "<p>Removed {$i} videos</p>";
-			}
-		}
-
-		$client = new Google_Client();
-		$client->setClientId($OAUTH2_CLIENT_ID);
-		$client->setClientSecret($OAUTH2_CLIENT_SECRET);
-		$client->setScopes('https://www.googleapis.com/auth/youtube');
-		$redirect = filter_var('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'],
-			FILTER_SANITIZE_URL);
-		$client->setRedirectUri($redirect);
-
-		// Define an object that will be used to make all API requests.
-		$youtube = new Google_Service_YouTube($client);
-
-		if (isset($_GET['code'])) {
-		  if (strval($_SESSION['state']) !== strval($_GET['state'])) {
-			die('The session state did not match.');
-		  }
-
-		  $client->authenticate($_GET['code']);
-		  $_SESSION['token'] = $client->getAccessToken();
-		  header('Location: ' . $redirect);
-		}
-
-		try {
-			if (isset($_SESSION['token'])) {
-			  $client->setAccessToken($_SESSION['token']);
-			}
-
-			// Check to ensure that the access token was successfully acquired.
-			if ($client->getAccessToken()) {
-
-			  $youtubeService = new Google_Service_YouTube($client);
-			  $subscriptions = $youtubeService->activities->listActivities('snippet,contentDetails', array('home' => true, 'maxResults' => 50, 'fields' => 'items(contentDetails,id,snippet)'));
-			  foreach($subscriptions as $sub) {
-				
-				$details = $sub->contentDetails;
-				$id = null;
-				if(isset($details->upload)) {
-					$id = $details->upload->videoId;
-				} elseif($show_recommendations && isset($details->recommendation)) {
-					$id = $details->recommendation->getResourceId()->videoId;
-				} elseif($show_recommendations && isset($details->bulletin)) {
-					$id = $details->bulletin->getResourceId()->videoId;
+				if($cmd) {
+					$batch = array();
+					$i = 0;
+					foreach($cmd as $c) {
+						$batch[$i++ % $download_threads][] = $c;
+					}
+					foreach($batch as $cmd) {
+						exec(implode(' && ', $cmd) . ' & disown');
+					}
+					$message .= "<p>Queued {$i} video(s) in " . count($batch) . " threads</p>";
+				}
+			} else {
+				if(isset($_POST['download'])) {
+					$i=0;
+					
+					$cmd = array();
+					foreach($_POST['download'] as $id => $download) {
+						if($download) {
+							if(isset($_POST['format'][$id]))
+								$format = $_POST['format'][$id];
+							else
+								$format = 18;
+							
+							$url = "http://www.youtube.com/watch?v={$id}";
+							$queued = @file_get_contents(__DIR__ . '/.youtube.downloaded');
+							$queued = json_decode($queued, true);
+							if(!$queued) $queued = array();
+							$queued[$id] = true;
+							file_put_contents(__DIR__ . '/.youtube.downloaded', json_encode($queued));
+							$cmd[] = "{$youtubedl_path} {$url} -f 18 3>/dev/null 2>/dev/null >/dev/null";
+							$i++;
+						}
+					}
+					
+					if($cmd) {
+						$batch = array();
+						$i = 0;
+						foreach($cmd as $c) {
+							$batch[$i++ % $download_threads][] = $c;
+						}
+						foreach($batch as $cmd) {
+							exec(implode(' && ', $cmd) . ' & disown');
+						}
+						$message .= "<p>Queued {$i} video(s) in " . count($batch) . " threads</p>";
+					}
 				}
 				
-				if($id && !isset($videos[$id])) {
-					$videos[$id] = array(
-						$sub->snippet->title,
-						$sub->snippet->thumbnails->default->url,
-						$sub->snippet->publishedAt,
-						$sub->snippet->channelTitle
-					);
+				if(isset($_POST['crap'])) {
+					$i=0;
+					foreach($_POST['crap'] as $id => $download) {
+						if($download) {
+							$queued = @file_get_contents(__DIR__ . '/.youtube.hidden');
+							$queued = json_decode($queued, true);
+							if(!$queued) $queued = array();
+							$queued[$id] = true;
+							file_put_contents(__DIR__ . '/.youtube.hidden', json_encode($queued));
+							$i++;
+						}
+					}
+					
+					if($i)
+						$message .= "<p>Removed {$i} video(s)</p>";
 				}
-			  }
-			  
-			  $queued = @file_get_contents(__DIR__ . '/.youtube.downloaded');
-			  $queued = json_decode($queued, true);
-			  if(!$queued) $queued = array();
+			}
+		}
+		
+		$alert = false;
+		$vidfile = __DIR__ . '/.youtube.videos';
+		
+		if(empty($_POST) && (!file_exists($vidfile) || !$use_channel_scan || time() - filemtime($vidfile) > 300)) {
+			$client = new Google_Client();
+			$client->setClientId($OAUTH2_CLIENT_ID);
+			$client->setClientSecret($OAUTH2_CLIENT_SECRET);
+			$client->setAccessType('offline');
+			$client->setApprovalPrompt('force');
+			$client->setApplicationName('Subfetcher');
+			$client->setScopes('https://www.googleapis.com/auth/youtube');
+			$redirect = filter_var('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'],
+				FILTER_SANITIZE_URL);
+			$client->setRedirectUri($redirect);
 
-			  $crap = @file_get_contents(__DIR__ . '/.youtube.hidden');
-			  $crap = json_decode($crap, true);
-			  if(!$crap) $crap = array();
+			try {
+				// Define an object that will be used to make all API requests.
+				$youtube = new Google_Service_YouTube($client);
 
-			  uasort($videos, function($a, $b) { return strtotime($b[2]) - strtotime($a[2]); });
-			  ob_start();
-			  ?>
-			  <style type="text/css">
-				table { width: 80%; margin: 0 auto; }
-				tr { opacity: 0.8; }
-				td.hide { background-color: #fff0f0; }
-				tr.checked { background-color: #efe; opacity: 1; }
-				tr.hidden { background-color: #fee; opacity: 0.4; } 
-				tr.checked td.hide { background-color: #efe; }
-				input.download, input.hide { display: block; margin: 0 auto; }
-				img { vertical-align: middle; padding: 0 24px; }
-				div.button { position: fixed; right: 10px; top: 10px; }
-			  </style>
-			  <form method="post" action="">
-				<div class="button">
-					<input type="submit" value="Download"/>
-				</div>
-				<table><thead><tr><th>Download</th><th>Image</th><th>Author</th><th>Name</th><th>Published</th><th>Hide</th></tr></thead><tfoot></tfoot><tbody>
-				<?php foreach($videos as $id => $data) { list($title, $image, $date, $author) = $data; if(isset($queued[$id]) || isset($crap[$id])) continue; $date = date('Y-m-d h:i:s', strtotime($date)); ?>
-					<tr><td><input class="download" type="checkbox" name="download[<?php echo $id ?>]"></td><td><img src="<?php echo $image ?>"></td><td><?php echo $author ?></td><td><?php echo $title ?></td><td><?php echo $date ?></td><td class="hide"><input type="checkbox" class="hide" name="crap[<?php echo $id ?>]"></td></tr>
-				<?php } ?>
-				</tbody></table>
-			  </form>
-			  <?php
-			  $htmlBody = ob_get_clean();
-			  $_SESSION['token'] = $client->getAccessToken();
+				if (isset($_GET['code'])) {
+				  if (strval($_SESSION['state']) !== strval($_GET['state'])) {
+					die('The session state did not match.');
+				  }
+
+				  $client->authenticate($_GET['code']);
+				  $_SESSION['token'] = $client->getAccessToken();
+				  header('Location: ' . $redirect);
+				}
+				
+				if (isset($_SESSION['token'])) {
+					$client->setAccessToken($_SESSION['token']);
+					
+					//json decode the session token and save it in a variable as object
+					$sessionToken = json_decode($_SESSION['token']);
+
+					//Save the refresh token (object->refresh_token) into a cookie called 'token' and make last for 1 month
+					if(isset($sessionToken->refresh_token))
+						setcookie('token', $sessionToken->refresh_token, time() + 86400 * 30);
+				} elseif(isset($_COOKIE['token'])) {
+					$client->refreshToken($_COOKIE['token']);
+				}
+
+				// Check to ensure that the access token was successfully acquired.
+				if ($client->getAccessToken()) {
+				  
+				  $youtubeService = new Google_Service_YouTube($client);
+				  if($use_channel_scan) {
+					  $channels = array();
+					  $subs = $youtubeService->subscriptions->listSubscriptions('snippet', array('mine' => true));
+					  foreach($subs as $sub) {
+						$channels[] = $sub->snippet->getResourceId()->channelId;
+					  }
+					  foreach($channels as $channel) {
+						$date = date('Y-m-d', strtotime('-5 days')) . 'T' . date('H:i:s') . 'Z';
+						$vids = $youtubeService->search->listSearch('snippet', array('channelId' => $channel, 'publishedAfter' => $date, 'maxResults' => 50, 'order' => 'date'));
+						foreach($vids as $vid) {
+							$videos[$vid->id->videoId] = array( 
+								$vid->snippet->title,
+								$vid->snippet->thumbnails->default->url,
+								$vid->snippet->publishedAt,
+								$vid->snippet->channelTitle,
+								'upload'
+							);
+						}
+					  }
+				  }
+				  
+				  if($use_activity_scan) {
+					  $subscriptions = $youtubeService->activities->listActivities('snippet,contentDetails', array('home' => true, 'maxResults' => 50, 'fields' => 'items(contentDetails,id,snippet)'));
+					  foreach($subscriptions as $sub) {
+						
+						$details = $sub->contentDetails;
+						$id = null;
+						if(in_array($type = $sub->snippet->type, $types)) {
+							if(isset($details->$type->videoId))
+								$id = $details->$type->videoId;
+							else
+								$id = $details->$type->getResourceId()->videoId;
+						}
+
+						if($id && !isset($videos[$id])) {
+							$videos[$id] = array(
+								$sub->snippet->title,
+								$sub->snippet->thumbnails->default->url,
+								$sub->snippet->publishedAt,
+								$sub->snippet->channelTitle,
+								$sub->snippet->type
+							);
+						}
+					  }
+				  }
+				  
+				  file_put_contents($vidfile, json_encode($videos));
+				  $_SESSION['token'] = $client->getAccessToken();
 			} else {
 			  // If the user hasn't authorized the app, initiate the OAuth flow
 			  $state = mt_rand();
 			  $client->setState($state);
 			  $_SESSION['state'] = $state;
-
 			  $authUrl = $client->createAuthUrl();
 			  $htmlBody = <<<END
 			  <h3>Authorization Required</h3>
@@ -237,24 +331,101 @@ END;
 			  $htmlBody = <<<END
 			  <h3>Authorization Required</h3>
 			  <p>You need to <a href="$authUrl">authorize access</a> before proceeding.<p>
+			  <audio src="alert.mp3" autoplay></audio>
 END;
 		}
-	}
-}	
-?>
+		  } else {
+			$videos = json_decode(file_get_contents($vidfile), true);
+		  }
+		  
+		  $queued = @file_get_contents(__DIR__ . '/.youtube.downloaded');
+		  $queued = json_decode($queued, true);
+		  if(!$queued) $queued = array();
 
+		  $crap = @file_get_contents(__DIR__ . '/.youtube.hidden');
+		  $crap = json_decode($crap, true);
+		  if(!$crap) $crap = array();
+
+		  $videos = array_diff_key($videos, $queued);
+		  $videos = array_diff_key($videos, $crap);
+
+		  $videos = array_filter($videos, function($e) {
+			return strtotime($e[2]) > strtotime('-2 days');
+		  });
+		  
+		  $alerts = @file_get_contents($alertfile = __DIR__ . '/.youtube.alerts');
+		  $alerts = json_decode($alerts, true);
+		  if(!$alerts) $alerts = array();
+		  if($diff = array_diff_key($videos, $alerts)) {
+			$alert = true;
+			file_put_contents($alertfile, json_encode(array_flip(array_keys($videos))));
+		  }
+		  
+		  uasort($videos, function($a, $b) { return strtotime($b[2]) - strtotime($a[2]); });
+		  if(empty($htmlBody)) {
+		  ob_start();
+		  if($alert) { ?>
+			<audio src="alert.mp3" autoplay></audio>
+		  <?php } ?>
+		  <style type="text/css">
+			table { width: 80%; margin: 0 auto; }
+			tr { opacity: 0.8; }
+			td.hide { background-color: #fff0f0; }
+			tr.checked { background-color: #efe; opacity: 1; }
+			tr.hidden { background-color: #fee; opacity: 0.4; } 
+			tr.checked td.hide { background-color: #efe; }
+			input.download, input.hide { display: block; margin: 0 auto; }
+			img { vertical-align: middle; padding: 0 24px; }
+			div.button { position: fixed; right: 10px; top: 10px; }
+		  </style>
+			<div class="button">
+				<input type="submit" value="Go"/>
+			</div>
+			<table><thead><tr><th>Download</th><th>Image</th><th>Author</th><th>Name</th><th>Published</th><th>Format</th><th>Post Type</th><th>Hide</th></tr></thead><tfoot></tfoot><tbody>
+			<?php foreach($videos as $id => $data) { list($title, $image, $date, $author, $type) = $data; $date = date('Y-m-d h:i:s', strtotime($date)); ?>
+				<tr><td><input class="download" type="checkbox" name="download[<?php echo $id ?>]"></td><td><img src="<?php echo $image ?>"></td><td><?php echo $author ?></td><td><?php echo $title ?></td><td><?php echo $date ?></td><td>
+				<select name="format[<?php echo $id ?>]">
+				<?php foreach($formats as $num => $format) { ?>
+					<option value="<?php echo $num ?>"><?php echo $format ?></option>
+				<?php } ?>
+				</select>
+				</td><td><?php echo ucfirst($type) ?></td><td class="hide"><input type="checkbox" class="hide" name="crap[<?php echo $id ?>]"></td></tr>
+			<?php } ?>
+			</tbody></table>
+		  <?php
+		  $htmlBody = ob_get_clean();
+		  }
+
+	}
+}
+
+?>
 <!doctype html>
 <html>
 <head>
 <title>Subscription Downloader</title>
 </head>
 <body>
-  <?php echo $message ?>
-  <?php if(!empty($htmlBody)) { echo $htmlBody; } ?>
+  <div class="downloading">
+	<?php echo $downloading ?>
+  </div>
+  <div class="message">
+	<?php echo $message ?>
+  </div>
+  <form method="post" action="">
+	<?php if(!empty($htmlBody)) { echo $htmlBody; } ?>
+	<label>Manual download by ID <input type="text" name="videoid" size="10"></label><br>
+  </form>
 </body>
 <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
 <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <script type="text/javascript">
+	var lastUpdate;
+	if (!Date.now) {
+	  Date.now = function now() {
+		return new Date().getTime();
+	  };
+	}
 	jQuery('tr').click(function() {
 		jQuery(this).find('input')[0].click();
 	});
@@ -271,6 +442,7 @@ END;
 		}
 	});
 	jQuery('input').change(function() {
+		lastUpdate = new Date();
 		var tr = jQuery(this).closest('tr');
 		if(tr.find('input:checked').is(':checked')) {
 			if(tr.find('input.download').is(':checked')) {
@@ -287,6 +459,25 @@ END;
 			tr.removeClass('checked');
 			tr.removeClass('hidden');
 		}
+	});
+	jQuery('select').click(function(e) {
+		lastUpdate = new Date();
+		e.stopPropagation();
+	});
+	
+	jQuery(function() {
+		lastUpdate = new Date();
+		
+		setTimeout(function() {
+			jQuery('div.message').hide();
+		}, 10000);
+		
+		setInterval(function() {
+			if(new Date().getTime() - lastUpdate.getTime() > 30000) {
+				lastUpdate = new Date();
+				window.location = window.location;
+			}
+		}, 5000);
 	});
 </script>
 </html>
